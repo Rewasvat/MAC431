beaa4a2593697cc5d2256d3347b962de701d71f0
Jefferson Ascaneo <jefferson.a@gmail.com>
2012-09-27 11:21:35 -0300

    Feita a fase 1

--- EP1/src/fase1.c
+++ EP1/src/fase1.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-void main() {
+int main() {
     long num_steps = 1000000;
     int i; double x, pi, step, sum = 0.0;
     step=1.0/(double)num_steps;
-#pragma omp parallel for reduction(+:sum)
+#pragma omp parallel for reduction(+:sum) private(x)
     for(i=0;i<num_steps;i++){
         x = (i+0.5)*step;
         sum += 4.0/(1.0+x*x);
     }
     pi = step*sum;
-    printf("PI = %g\n", pi);
+    printf("PI = %.20g\n", pi);
+    return 0;
 }
